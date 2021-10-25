cc37f88a935bac769e0bb10cd08647e60cc4f9b0
Dennis <dennis.baumann@hotmail.com>
2021-10-25 14:27:44 +0200

    first commit

---
+++ public/js/editor.js
@@ -0,0 +1,79 @@
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
+
+//banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
+
+
+
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput, "image");
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+    const[file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new Formdata();
+        formdata.append('image', file);
+
+        fetch('/upload', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            } else{
+                bannerPath = `${location.origin}/${data}`;
+                banner.style.backgroundImage = `url("${bannerPath}")`;
+            }
+        })
+    }else{
+        alert("upload Image only")
+    }
+}
+
+// This function will let you insrert a text format of your image of example if I upload 1.png then 
+// this function insert something like thie ![1.png](image path) inside our article field.
+const addImage = (imagepath, alt) => {
+    let curPos = articleFeild.selectionStart;
+    let textToInsert = `\r![${alt}](${imagepath})\r`;
+    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleFeild.value.length && blogTitleField.value.length){
+        // generating id
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let id = "";
+        for(let i = 0; i < 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
+        }
+
+        // setting up docName
+        let docName = `${blogTitle}-${id}`;
+        let date = new Date(); // for published at info
+
+        //access firstore with db variable;
+        db.collection("Freiraum").doc(docName).set({
+            title: blogTitleField.value,
+            article: articleFeild.value,
+            bannerImage: bannerPath,
+            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() =>{
+            location.href = `/${docName}`;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+})
---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCylEWnSVBIMcqJltMocvhDvs5oHJ9s7BM",
+  authDomain: "freiraum-blog.firebaseapp.com",
+  projectId: "freiraum-blog",
+  storageBucket: "freiraum-blog.appspot.com",
+  messagingSenderId: "767556444959",
+  appId: "1:767556444959:web:eb4f90c543b36ddb87862a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
