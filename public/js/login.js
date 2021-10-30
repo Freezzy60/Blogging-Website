b855a1bface59f8a0194d6a53deaa18f40811aa1
Pascal Pozzera <pascal.e.pozzera@gmail.com>
2021-10-30 14:47:54 +0200

    login/logout

--- public/js/login.js
+++ public/js/login.js
@@ -1,119 +1,52 @@
-// Set up our login function
-function login(editorId, dialogId) {
-  // Get all our input fields
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
+//Sign Up User
+function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-      alert("Email or Password is Outta Line!!");
-      return;
-      // Don't continue running the code
-  }
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
 
-  auth
-      .signInWithEmailAndPassword(email, password)
-      .then(function () {
-          // Declare user variable
-          var user = auth.currentUser;
-
-          // Add this user to Firebase Database
-          var database_ref = database.ref();
-
-          // Create User data
-          var user_data = {
-              last_login: Date.now(),
-          };
-
-          // Push to Firebase Database
-          database_ref.child("users/" + user.uid).update(user_data);
-
-          document.getElementById(editorId).classList.add("editor-active");
-          document.getElementById(dialogId).classList.remove("active");
-
-          
-      })
-      .catch(function (error) {
-          // Firebase will use this to alert of its errors
-          var error_code = error.code;
-          var error_message = error.message;
-
-          alert(error_message);
-      });
+    promise.catch(e => alert(e.message));
 }
 
-// Validate Functions
-function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/;
-  if (expression.test(email) == true) {
-      // Email is good
-      return true;
-  } else {
-      // Email is not good
-      return false;
-  }
+//Loggin User
+function signIn () {
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+
+    document.getElementById("login").classList.remove("active")
+
+    promise.catch(e => alert(e.message));
 }
 
-function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password < 6) {
-      return false;
-  } else {
-      return true;
-  }
+//Loggout User
+function signOut() {
+    auth.signOut();
 }
 
-// Set up our register function
-function register() {
-  // Get all our input fields
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
+//User signed in ?
+auth.onAuthStateChanged(function(user,editorId,loginId){
+    if(user){
+        var email = user.email;
+        document.getElementById("active-login-editor").classList.add("active");
+        document.getElementById("active-login-logout").classList.add("active");
+        document.getElementById("inactive-login").classList.add("inactive");
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-      alert("Email or Password is Outta Line!!");
-      return;
-      // Don't continue running the code
-  }
-
-  // Move on with Auth
-  auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(function () {
-          // Declare user variable
-          var user = auth.currentUser;
-
-          // Add this user to Firebase Database
-          var database_ref = database.ref();
-
-          // Create User data
-          var user_data = {
-              email: email,
-              last_login: Date.now(),
-          };
-
-          // Push to Firebase Database
-          database_ref.child("users/" + user.uid).set(user_data);
-
-          // DOne
-          alert("User Created!!");
-      })
-      .catch(function (error) {
-          // Firebase will use this to alert of its errors
-          var error_code = error.code;
-          var error_message = error.message;
-
-          alert(error_message);
-      });
-}
+    }else{
+        document.getElementById("editor-nav").classList.remove("active");
+    }
+});
 
 
 //Show PopUp -> add .active to classlist
 function openPopUp(dialogId) {
-  document.getElementById(dialogId).classList.add("active");
-}
-
+    document.getElementById(dialogId).classList.add("active");
+  }
+  
 //Hide PopUp -> remove .avtive from classlist
-function closePopUp(dialogId) {
-  document.getElementById(dialogId).classList.remove("active");
-}
+  function closePopUp(dialogId) {
+    document.getElementById(dialogId).classList.remove("active");
+  }
+  
