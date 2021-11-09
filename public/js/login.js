47f81b94000611f3d0e4fe6874cbf800833dd221
Pascal Pozzera <pascal.e.pozzera@gmail.com>
2021-11-09 13:55:55 +0100

    login überarbeitet

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,37 @@
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
 import { getDatabase, ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";
 
 //Auth
 const auth = getAuth();
 //Database
 const db = getDatabase();
+//set persistence to session
+auth.setPersistence().Session();
 
 //References
 let openLoginBtn = document.getElementById("openLogin");
 let closeLogin = document.getElementById("closeLogin");
 let loginBtn = document.getElementById("loginBtn");
+let logoutBtn = document.getElementById("logoutBtn")
 let signUpBtn = document.getElementById("signUpBtn");
+
+//
+//User logged in ?
+//
+
+auth.onAuthStateChanged(function (user) {
+    if (user) {
+        //Remove active -> opactity 0 (login/signup)
+        document.getElementById("login").classList.remove("active");
+        //Remove login btn if logged in
+        document.getElementById("openLogin").classList.add("in-active");
+        //Show btn editor
+        document.getElementById("editorBtn").classList.add("activeEditorBtn");
+        //Show btn logout
+        document.getElementById("logoutBtn").classList.add("activeLogoutBtn");
+    }
+})
+
 
 //Show PopUp -> add .active to classlist
 function openPopUp() {
@@ -26,9 +47,12 @@ function closePopUp(dialogId) {
 
 closeLogin.addEventListener('click', closePopUp);
 
+//
 //Login User with email
+//
 
-function login() {
+
+function signIn() {
 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -39,11 +63,10 @@ function login() {
         return;
     }
 
-
     signInWithEmailAndPassword(auth, email, password)
         .then(function () {
-            
-            update(ref(db, "admin/" + auth.currentUser.uid),            
+
+            update(ref(db, "admin/" + auth.currentUser.uid),
                 {
                     email: email,
                     last_login: Date.now(),
@@ -58,11 +81,81 @@ function login() {
             alert(error_message);
 
         })
+
+    //Remove active -> opactity 0 (login/signup)
+    document.getElementById("login").classList.remove("active");
+    //Remove login btn if logged in
+    document.getElementById("openLogin").classList.add("in-active");
+    //Show btn editor
+    document.getElementById("editorBtn").classList.add("activeEditorBtn");
+    //Show btn logout
+    document.getElementById("logoutBtn").classList.add("activeLogoutBtn");
+
 };
 
-loginBtn.addEventListener('click', login);
+//Onclick Sign In button -> login user
+loginBtn.addEventListener('click', signIn);
 
+//
+//Create User with email and password
+//
+
+
+function signUp() {
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert("Email or Password is Outta Line!!");
+        return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(function () {
+
+            set(ref(db, "admin/" + auth.currentUser.uid),
+                {
+                    email: email,
+                    last_login: Date.now(),
+                })
+        })
+        //Catch wrong Login
+        .catch(function (error) {
+            // Firebase will use this to alert of its errors
+            var error_code = error.code;
+            var error_message = error.message;
+
+            alert(error_message);
+
+        })
+}
+
+//Sign Up Button on click -> create user
+signUpBtn.addEventListener('click', signUp);
+
+
+//
+//Logout
+//
+
+function logout() {
+
+    auth.signOut().then(function () {
+        //Remove login btn if logged in
+        document.getElementById("openLogin").classList.remove("in-active");
+        document.getElementById("editorBtn").classList.remove("activeEditorBtn");
+        document.getElementById("logoutBtn").classList.remove("activeLogoutBtn");
+    })
+}
+
+//Logout button on click -> logout user
+logoutBtn.addEventListener('click', logout)
+
+//
 // Check Email
+//
 function validate_email(email) {
     let expression = /^[^@]+@\w+(\.\w+)+\w$/;
     if (expression.test(email) == true) {
@@ -73,7 +166,10 @@ function validate_email(email) {
         return false;
     }
 }
+
+//
 //Check Password bigger 6 ?
+//
 function validate_password(password) {
 
     if (password < 6) {
